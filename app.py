804bcf84c33b7872e03cedf19f14e0ed86c15d4b
AntLouiz <luizrodrigo46@hotmail.com>
2021-01-09 15:52:51 -0300

    Add settings module

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import motor
 from tornado.web import Application
 from core.urls import urlpatterns as core_urlpatterns
-
-DATABASE_URI = 'mongodb://localhost:27017'
-DATABASE_NAME = 'core'
+from settings import DATABASE_NAME, DATABASE_URI
 
 def create_app():
     db_client = motor.motor_tornado.MotorClient(DATABASE_URI)
