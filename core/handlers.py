56d1834c56d5bb6dd9c84ff1c4afda6bda15b7b5
AntLouiz <luizrodrigo46@hotmail.com>
2021-01-09 19:24:05 -0300

    Update query with new sort arg

--- core/handlers.py
+++ core/handlers.py
@@ -1,5 +1,6 @@
 from base.handlers import ModelAPIView
 from core.models import Person, Customer
+from pymongo import ASCENDING
 
 
 class PersonModelAPIView(ModelAPIView):
@@ -7,14 +8,18 @@ class PersonModelAPIView(ModelAPIView):
     lookup_field = '_id'
     lookup_url_kwarg = 'object_id'
 
+    async def get_queryset(self, many=True, *args, **kwargs):
+        sort_rule = ('name', ASCENDING)
+        queryset = await self.model.manager.find(
+            self.query_filter,
+            many=many,
+            sort=sort_rule
+        )
+        return queryset
+
     async def list(self, *args, **kwargs):
         queryset = await self.get_queryset(*args, **kwargs)
         response = self.process_response(queryset)
-
-        customer = Customer(self.db)
-
-        print(await customer.manager.find())
-
         response = self.paginate_response(queryset)
         return self.json_response(data=response)
 
