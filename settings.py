8909e5f0ea268f6d41cbe0e1745ce71c4afce004
AntLouiz <luizrodrigo46@hotmail.com>
2021-01-09 18:08:53 -0300

    Add logs

--- settings.py
+++ settings.py
@@ -1,2 +1,26 @@
+import sys
+import logging
+import os.path
+from decouple import config
+
+
+BASE_DIR = os.path.dirname(__file__)
+LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+
+DEBUG = config('DEBUG', default=True)
+
+level = logging.INFO
+
+if DEBUG:
+    level = logging.DEBUG
+
+logging.basicConfig(filename=os.path.join(LOGS_DIR, 'app.log'), filemode='a', level=level)
+logger = logging.getLogger()
+
+logging.getLogger('asyncio').setLevel(logging.WARNING)
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+logger.addHandler(stdout_handler)
+
 DATABASE_URI = 'mongodb://localhost:27017'
 DATABASE_NAME = 'core'
--- start.py
+++ start.py
@@ -1,10 +1,12 @@
 import unittest
 from tornado.ioloop import IOLoop
 from app import create_app
+from settings import logger
 
 
 if __name__ == '__main__':
     app = create_app()
     port = 8888
+    logger.info(f'Listening on port {port} ...')
     app.listen(port)
     IOLoop.current().start()
