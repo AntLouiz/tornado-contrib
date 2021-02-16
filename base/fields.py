40f93539454fc57a7c596ef949ef61bbe262630b
AntLouiz <luizrodrigo46@hotmail.com>
2021-02-16 08:36:47 -0300

    Add ObjectIdType field

--- base/fields.py
+++ base/fields.py
@@ -1,1 +1,19 @@
+import bson
 from schematics.types import *
+from schematics.common import *
+from schematics.exceptions import ConversionError
+from schematics.contrib.mongo import ObjectIdType
+
+
+class ObjectIdType(ObjectIdType):
+
+    def to_native(self, value, context=None):
+        if not isinstance(value, bson.objectid.ObjectId) or not isinstance(value, str):
+            try:
+                value = bson.objectid.ObjectId(str(value))
+            except bson.objectid.InvalidId:
+                try:
+                    value = value['$oid']
+                except KeyError:
+                    raise ConversionError(self.messages['convert'])
+        return value
